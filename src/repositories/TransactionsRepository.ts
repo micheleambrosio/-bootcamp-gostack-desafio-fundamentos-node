b1842c47112269fa816ff3c808147198ce949154
Michele Ambrosio <michele.ambrosio@eduzz.com>
2020-08-31 18:52:00 -0300

    Create transactions and list transactions with balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactionValues = this.transactions.filter(transaction =>
+      transaction.type === 'income'
+    ).map(transaction => transaction.value);
+
+    const income: number = incomeTransactionValues.reduce((a, b) => a + b, 0);
+
+    const outcomeTransactionValues = this.transactions.filter(transaction =>
+      transaction.type === 'outcome'
+    ).map(transaction => transaction.value);
+
+    const outcome: number = outcomeTransactionValues.reduce((a, b) => a + b, 0);
+
+    return {
+      income,
+      outcome,
+      total: income - outcome
+    }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
